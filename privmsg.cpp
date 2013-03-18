af4516433ba2edf51d53cc9b0e9856954d382204
Kyle Fuller <inbox@kylefuller.co.uk>
2013-03-19 03:43:49 +0700

    privmsg: Fix indentation and allow module to globally be loaded

--- privmsg.cpp
+++ privmsg.cpp
@@ -11,25 +11,30 @@
 
 class CPrivMsgMod : public CModule {
 public:
-        MODCONSTRUCTOR(CPrivMsgMod) {}
+	MODCONSTRUCTOR(CPrivMsgMod) {}
 
-        virtual EModRet OnUserMsg(CString& sTarget, CString& sMessage) {
-                if (m_pNetwork && m_pNetwork->GetIRCSock() && !m_pNetwork->IsChan(sTarget)) {
-                        m_pNetwork->PutUser(":" + m_pNetwork->GetIRCNick().GetNickMask() + " PRIVMSG " + sTarget + " :" + sMessage, NULL, m_pClient);
-                }
+	virtual EModRet OnUserMsg(CString& sTarget, CString& sMessage) {
+		if (m_pNetwork && m_pNetwork->GetIRCSock() && !m_pNetwork->IsChan(sTarget)) {
+			m_pNetwork->PutUser(":" + m_pNetwork->GetIRCNick().GetNickMask() + " PRIVMSG " + sTarget + " :" + sMessage, NULL, m_pClient);
+		}
 
-                return CONTINUE;
-        }
+		return CONTINUE;
+	}
 
-        virtual EModRet OnUserAction(CString& sTarget, CString& sMessage) {
-                if (m_pNetwork && m_pNetwork->GetIRCSock() && !m_pNetwork->IsChan(sTarget)) {
-                        m_pNetwork->PutUser(":" + m_pNetwork->GetIRCNick().GetNickMask() + " PRIVMSG " + sTarget + " :\x01" + "ACTION " + sMessage + "\x01", NULL, m_pClient);
-                }
+	virtual EModRet OnUserAction(CString& sTarget, CString& sMessage) {
+		if (m_pNetwork && m_pNetwork->GetIRCSock() && !m_pNetwork->IsChan(sTarget)) {
+			m_pNetwork->PutUser(":" + m_pNetwork->GetIRCNick().GetNickMask() + " PRIVMSG " + sTarget + " :\x01" + "ACTION " + sMessage + "\x01", NULL, m_pClient);
+		}
 
-                return CONTINUE;
-        }
-
+		return CONTINUE;
+	}
 };
+
+template<> void TModInfo<CPrivMsgMod>(CModInfo& Info) {
+	Info.SetWikiPage("privmsg");
+	Info.AddType(CModInfo::NetworkModule);
+	Info.AddType(CModInfo::GlobalModule);
+}
 
 USERMODULEDEFS(CPrivMsgMod, "Send outgoing PRIVMSGs and CTCP ACTIONs to other clients")
 
